a17f1e1e7383111c0838fbbd1295c346f39c3a8f
aadesh <aadeshrn@gmail.com>
2020-04-30 15:59:56 +0530

    uploaded files

---
+++ script.js
@@ -0,0 +1,55 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+var Textbox = $('#textbox');
+var instructions = $('instructions');
+
+var Content = '';
+
+recognition.continuous = true;
+
+recognition.onresult = function(event) {
+
+  var current = event.resultIndex;
+
+  var transcript = event.results[current][0].transcript;
+ 
+    Content += transcript;
+    Textbox.val(Content);
+	//draw();
+	//var queryString="?para1=" +Content;
+	localStorage.setItem('str',Content);
+	window.location.replace("index.html");
+	
+	
+
+
+  
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition is ON.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('No activity.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('Try again.');  
+  }
+}
+
+$('#start-btn').on('click', function(e) {
+  if (Content.length) {
+    Content += ' ';
+  }
+  recognition.start();
+});
+
+Textbox.on('input', function() {
+  Content = $(this).val();
+})
+
